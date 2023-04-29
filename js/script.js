e5fa9b45ea597838e0a382993f1096ea77de642c
staadegh <33016148+staadegh@users.noreply.github.com>
2023-04-28 21:09:23 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let pokemonList =[{name: 'Bulbasaur', height: 34, types:['Leave', 'Tree'], Abilities:"Shed-skin", EVs:"2 Defence"},
+                  {name: 'Dynosaur', height: 56, types:['Green', 'Sour'], Abilities: "Run-away", EVs: "1 Speed"},
+                  {name: 'Elephant', height: 48, types:['Tall', 'Short'], Abilities:"Shed-skin", EVs:"2 Speed"},
+                  {name: 'Horsey', height: 41, types:['Big', 'Finest'], Abilities:"Shed-skin", EVs:"3 Speed" },
+                  {name: 'Horsey', height: 41, types:['Big', 'Finest'], Abilities:"Shed-skin", EVs:"3 Speed" }];
